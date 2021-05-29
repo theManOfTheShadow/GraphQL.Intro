97d2d09342e70eb165b8c525a0b6f20cfeffff3f
michael1834 <michal.parzych@hotmail.com>
2021-05-29 20:12:28 +0200

    up

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -31,7 +31,7 @@ namespace WebApplication1
                 .AddMutationType<Mutation>()
                 .Create()
                 );
-        }
+        } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
